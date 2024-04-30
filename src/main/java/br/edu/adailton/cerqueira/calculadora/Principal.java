4e314b0d7805ad15706383ff2a31772feb2152fc
ADAILTON DE JESUS CERQUEIRA JUNIOR <adailton.cerqueira@ulife.com.br>
2024-04-30 20:28:49 -0300

    Adicionando trecho da subtração.

--- src/main/java/br/edu/adailton/cerqueira/calculadora/Principal.java
+++ src/main/java/br/edu/adailton/cerqueira/calculadora/Principal.java
@@ -2,9 +2,10 @@ package br.edu.adailton.cerqueira.calculadora;
 
 public class Principal {
 
-	public static void main(String[] args) {
-		var calculadora = new Calculadora();
-		System.out.println("CALCULADORA");
-		System.out.println(calculadora.somar(1d, 2d));
-	}
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        System.out.println("CALCULADORA");
+        System.out.println("Somando: " + calculadora.somar(1d, 2d));
+        System.out.println("Subtraindo: " + calculadora.subtrair(1d, 2d));
+    }
 }
